31117e9f3f6ea27f878728fa9a7d833abc91aaa9
Schakko <schakkonator@googlemail.com>
2012-08-29 09:10:45 +0200

    Added regular expression test

---
+++ NeosIT.DB-Migrator.Test/DBMigration/Target/MSSQL/DbInterfaceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace NeosIT.DB_Migrator.Test.DBMigration.Target.MSSQL
+{
+    public class DbInterfaceTest
+    {
+        [Test]
+        public void TestRegExp()
+        {
+            var s = " 20120820 001      \r";
+            Match match = Regex.Match(s, @"\s*(\d*)\s*(\d*)\s*");
+
+            Assert.AreEqual("20120820", match.Groups[1].ToString());
+            Assert.AreEqual("001", match.Groups[2].ToString());
+        }
+    }
+}
